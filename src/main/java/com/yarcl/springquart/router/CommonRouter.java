8b9c5c99ef218295929b0d210974442b9504cf94
yarcl <yarcl@sina.cn>
2019-03-02 23:06:44 +0800

    优化了下路由内容

--- src/main/java/com/yarcl/springquart/router/CommonRouter.java
+++ src/main/java/com/yarcl/springquart/router/CommonRouter.java
@@ -1,4 +1,4 @@
-package com.yarcl.springquart.controller;
+package com.yarcl.springquart.router;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +14,20 @@ import java.io.IOException;
  * Created by Administrator on 2018/1/10.
  */
 @RestController
-@RequestMapping(value="/comm")
-public class CommonController {
+@RequestMapping(value="/router")
+public class CommonRouter {
 
+    @GetMapping(value={"/", "login"}, produces = {"application/json;charset=utf-8"})
+    public ModelAndView routToLogin() throws IOException {
+        ModelAndView  mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
 
     @GetMapping(value="/index", produces = {"application/json;charset=utf-8"})
     public ModelAndView routeToIndex() throws IOException {
         ModelAndView  mav = new ModelAndView();
-        mav.setViewName("login");
+        mav.setViewName("main/index");
         return mav;
     }
 
